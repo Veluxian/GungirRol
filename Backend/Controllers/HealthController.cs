56f8dd5badc2460e89b1c28f64f59486b5df0ab8
Joaquin Orellana <joaquinosotelo@gmail.com>
2026-05-18 23:06:27 -0400

    Eliminamos el weather de prueba y agregamos un healthController antes de comenzar

---
+++ Backend/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController: ControllerBase
+    {
+        [HttpGet]
+        public IActionResult CheckStatus()
+        {
+            return Ok(new
+            {
+                Status = "Healty",
+                message = "Servidores Activos",
+                timestamp = DateTime.UtcNow
+            });
+        }
+    }
+}
