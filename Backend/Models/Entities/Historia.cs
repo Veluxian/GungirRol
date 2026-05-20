83dacb92ea2ebe510013ee3410c58b1fe91bedb5
Joaquin Orellana <joaquinosotelo@gmail.com>
2026-05-19 23:04:13 -0400

    creamos la entidad de historia para probar

--- Backend/Models/Entities/Historia.cs
+++ Backend/Models/Entities/Historia.cs
@@ -1,6 +1,26 @@
-﻿namespace Backend.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Models.Entities
 {
+    [Table("Historias")]
     public class Historia
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Categoria { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string Nombre {  get; set; } = string.Empty;
+        [Required]
+        [MaxLength(1000)]
+        public string Descripcion { get; set; } = string.Empty;
+        [MaxLength(500)]
+        public string DatoExtra {  get; set; } = string.Empty;
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
     }
 }
