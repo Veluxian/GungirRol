0b7bc0d13530fa7100e6cce56dd95d69da1ceb8f
Joaquin Orellana <joaquinosotelo@gmail.com>
2026-05-22 15:56:16 -0400

    comenzamos a hacer las conexiones con la base de datos dejando el DockerCompose para el PostgreSQL

---
+++ Backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Backend.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationsDbContext> options): base(options) { }
+        public DbSet<Historia> Historias { get; set; }
+    }
+}
